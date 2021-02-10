99f342ce323a81c7a3a7087d2fddecb08e1046f6
Karina <kstifurska@gmail.com>
2021-02-10 14:57:45 +0200

    Pievienoju Scanner un DecimalFormat

--- src/Stifurska_pica.java
+++ src/Stifurska_pica.java
@@ -1,4 +1,7 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Stifurska_pica {
-
+	static Scanner scan = new Scanner(System.in);
+	static DecimalFormat df = new DecimalFormat("#.##");
 }
