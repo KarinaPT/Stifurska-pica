488235a52a3d2b328d9e233bb4616d9b8ee8e292
Karina <kstifurska@gmail.com>
2021-02-11 09:59:49 +0200

    veicu divas metodes un static void

--- src/Stifurska_pica.java
+++ src/Stifurska_pica.java
@@ -7,6 +7,7 @@ public class Stifurska_pica {
 	
 	static Scanner scan = new Scanner(System.in);
 	static DecimalFormat df = new DecimalFormat("#.##");
+	static String vards, adrese, talruni,teksts, picasnosaukums, atb,lielums,izvele, maksasanasveids,pica;
 	
 	public static void Izveidot(int a, String nosaukums1) {
 		
@@ -21,32 +22,35 @@ public class Stifurska_pica {
 		int klienti;
 		do{
 			
-			izvele = JOptionPane.showInputDialog("'MENU' - veiciet pasūtījumu no menu; 'ManaPica' - izveido savu picu");
+			izvele = JOptionPane.showInputDialog("'MENU' - veikt pasūtījumu no menu; 'ManaPica' - izveido savu picu");
 			izvele = izvele.toUpperCase();
 			
 			if(izvele.equals("MENU")){
 				
-				System.out.println("Здравствуйте!");
-				System.out.println("Вы выбрали услугу - заказ по меню. Наше заведение предлагает Вам различные пицы одних размеров и одной ценой.");
-				System.out.println("Весь Ваш заказ будет сохранен и показан у Вас на рабочем столе");
-				System.out.println("Всего хорошего!");
+				System.out.println("Sveiki!");
+				System.out.println("Jūs esat izvēlējies pakalpojumu - veikt pasūtījumu no menu. Mūsu uzņēmums piedāvā dažādas vienāda lieluma un cenas picas.");
+				System.out.println("Pasūtījuma beigās jūs saņemsiet čeku");
+				System.out.println("Visu labu!");
 				
-				System.out.println("Введите название заказа: ");
+				System.out.println("Ievadiet pasūtījuma nosaukumu: ");
 				nosaukums1 = scan.next();
 				
 				System.out.println("Cik daudz klientu vēlas veikt pasūtījumu? ");
 				klienti = scan.nextInt();
 				
-				System.out.println("Сколько пиц будет заказанно?");
+				System.out.println("Cik picas vēlaties pasūtīt?");
 				int a = scan.nextInt();
 				scan.close();
 				
 				for(int i=1; i<=klienti; i++) {
 				Izveidot(a, nosaukums1);
+				
 				}
 			}else if (izvele.equals("MANAPICA")) {
-				System.out.println("Введите название заказа: ");
+				
+				System.out.println("Ievadiet pasūtījuma nosaukumu: ");
 				nosaukums2 = scan.next();
+				
 				Menu(nosaukums2);
 				}else{
 				JOptionPane.showMessageDialog(null, "Sāda darbība nepastāv!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
